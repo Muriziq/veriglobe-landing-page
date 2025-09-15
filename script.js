9ad09b5dde749fe0375e77fea85b9f914f57096d
Muriziq <murizikthanni@gmail.com>
2025-09-15 13:10:03 -0600

    animation on index

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 function firstPage(){
     const header = document.querySelector("header")
-    const headerTimeline = gsap.timeline({defaults:{ease:"power.inOut",duration:1}})
+    const headerTimeline = gsap.timeline({defaults:{ease:"power.inOut",duration:0.7}})
     headerTimeline.to(header,{opacity:1})
+    headerTimeline.fromTo(header.querySelector("img"),{opacity:0,x:-30},{opacity:1,x:0})
+    headerTimeline.fromTo(header.querySelectorAll("button"),{opacity:0,y:30},{opacity:1,y:0,stagger:0.2},"-=0.5")
+    headerTimeline.fromTo(header.querySelectorAll("a"),{opacity:0,y:30,x:30},{opacity:1,y:0,x:0,stagger:{each:0.1,from:"center"}},"-=0.5")
 
+    const firstPageTimeline = gsap.timeline({defaults:{ease:"power.inOut",duration:0.7}})
+    const firstPage = document.querySelector(".first-article")
+    firstPageTimeline.fromTo(firstPage.querySelector("svg"),{opacity:0,width:0,height:0},{opacity:1,width:"100%",height:"100%"})
+firstPageTimeline.fromTo(firstPage.querySelectorAll("section > div, section > h1, section > p, section > button")
+,{opacity:0,y:30},{opacity:1,y:0,stagger:0.2},"-=0.5")
+
+
+const secondArticle = document.querySelector(".second-article")
+gsap.fromTo(secondArticle.querySelectorAll("div img"),{opacity:0,y:100},{opacity:1,y:0,duration:1,scrollTrigger:{trigger:secondArticle,start: "top 90%",end:"bottom top",toggleActions:"play none resume none"}})
+const secondArticleSec = secondArticle.querySelector("section")
+gsap.fromTo(secondArticleSec.querySelector("img"),{opacity:0,x:-100},{opacity:1,x:0,scrollTrigger:{trigger:secondArticleSec,start:"top 80%",end:"bottom top",toggleActions:"play none resume none"}})
+gsap.fromTo(secondArticleSec.querySelector("DIV"),{opacity:0,y:100},{opacity:1,y:0,scrollTrigger:{trigger:secondArticleSec,start:"top 80%",end:"bottom top",toggleActions:"play none resume none"}})
+
+gsap.fromTo(document.querySelectorAll(".third-article div"),{opacity:0,y:50},{opacity:1,y:0,ease:"power1.inOut",scrollTrigger:{trigger:document.querySelector(".third-article section"),start:"top 80%",end:"bottom top",toggleActions:"play none resume none"},duration:0.5,stagger:0.2})
+const fourthArticleDivs = Array.from(document.querySelectorAll(".fourth-article > section > div"))
+gsap.fromTo(fourthArticleDivs[0],{opacity:0,x:-150},{opacity:1,x:0,scrollTrigger:{trigger:document.querySelector(".fourth-article > section"),start:"top 80%",end:"bottom top",toggleActions:"play none resume none"},duration:1})
+gsap.fromTo(fourthArticleDivs[1],{opacity:0,y:150},{opacity:1,y:0,scrollTrigger:{trigger:document.querySelector(".fourth-article > section"),start:"top 80%",end:"bottom top",toggleActions:"play none resume none"},duration:1})
+gsap.fromTo(fourthArticleDivs[2],{opacity:0,x:150},{opacity:1,x:0,scrollTrigger:{trigger:document.querySelector(".fourth-article > section"),start:"top 80%",end:"bottom top",toggleActions:"play none resume none"},duration:1})
+
+gsap.fromTo(document.querySelectorAll(".fifth-article h2,.fifth-article > div p"),{opacity:0,y:40},{opacity:1,y:0,stagger:0.2,scrollTrigger:{trigger:".fifth-article",start:"top 80%",end:"bottom top",toggleActions:"play none resume none"}})
+
+gsap.fromTo(".sixth",{opacity:0,y:100},{opacity:1,y:0,scrollTrigger:{trigger:document.querySelector(".sixth-article section"),start:"top 90%",end:"bottom top",toggleActions:"play none resume none"},duration:1})
+gsap.fromTo(".sixth1",{opacity:0,x:100},{opacity:1,x:0,scrollTrigger:{trigger:document.querySelector(".sixth-article section"),start:"top 90%",end:"bottom top",toggleActions:"play none resume none"},duration:1})
 }
+gsap.fromTo(".seventh-article",{opacity:0,y:100},{opacity:1,y:0,ease:"power2.inOut",scrollTrigger:{trigger:".seventh-article",toggleActions:"play none resume none",start:"top 80%"},duration:1})
+
+gsap.fromTo(document.querySelector("footer section"),{opacity:0,x:-100},{opacity:1,x:0,scrollTrigger:{trigger:document.querySelector("footer section"),start:"top 90%",toggleActions:"play none resume none"},duration:1})
+gsap.fromTo(document.querySelectorAll("footer nav"),{opacity:0,y:100},{opacity:1,y:0,scrollTrigger:{trigger:document.querySelector("footer section"),start:"top 90%",toggleActions:"play none resume none"},duration:1})
